1837f3e4cbfb54819e114cdab19e065bacafc67a
Pragya <pragyaaa620@gmail.com>
2025-02-14 23:31:35 +0530

    firebase changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTPS7GuZDLBUy9WWsYEvRuXJXUkb7SfCw",
-  authDomain: "track-it-ai.firebaseapp.com",
-  projectId: "track-it-ai",
-  storageBucket: "track-it-ai.firebasestorage.app",
-  messagingSenderId: "380749338926",
-  appId: "1:380749338926:web:e60e2644e04ab82c6e42bf",
-  measurementId: "G-RRPL30VY0H"
+  apiKey: import.meta.env.VITE_FIREBASE_apikey,
+  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+  projectId: import.meta.env.VITE_FIREBASE_projectId,
+  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_appId,
+  measurementId: import.meta.env.VITE_FIREBASE_measurementId
 };
 
 // Initialize Firebase
